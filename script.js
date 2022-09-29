7df44f60631ecd7a796adcc237aea61276393c82
Bryan Valencia <bryan.val93@gmail.com>
2022-09-29 08:40:43 -0400

    reworked layout a bit, changed elements where API info displays to dynamic

--- script.js
+++ script.js
@@ -21,15 +21,24 @@
 // correct it for better appearance
 
 // Streeetchy Stretch: Rewrite back end in Ruby
+$(function() {
+// var myModal = new bootstrap.Modal(document.getElementById('encounterModal'));
 
-var pokeNameDiv = document.getElementById('pokeName');
-var pokeImgDiv = document.getElementById('pokeImgDiv');
-var pokeInfoDiv = document.getElementById('pokeInfoDiv');
+let pokeIntroDiv = document.getElementById('pokeIntro');
+let welcomeText = document.getElementsByClassName('welcome-text');
+let pokeNameDiv = document.getElementById('pokeName');
+let pokeImgDiv = document.getElementById('pokeImgDiv');
+let pokeInfoDiv = document.getElementById('pokeInfoDiv');
+
 var button = document.getElementById('pokeGet');
+
+// pokeDetailsDiv.appendChild(pokeInfoDiv);
 // nesting the function to be called inside an outer generic function
 // makes it so that the onclick/fetch doesn't execute on page load
 button.onclick = function() {
+    $('.welcome-text').remove();
     var randPkmnNum = Math.floor(Math.random()* 807);
+    // myModal.show();
     fetchFn(randPkmnNum);
 };
 
@@ -38,7 +47,17 @@ button.onclick = function() {
 // using response.json() to make get the data in json
 
 function fetchFn (pkmnNum) {
-    var img =  document.createElement('img');
+    $('.pokeNameDiv').remove();
+    $('.pokeType').remove();
+    $('.pokeImgDiv').remove();
+    $('.pokeInfoDiv-flvr-text').remove();
+    $('.pokeInfoDiv-stats').remove();
+
+    // let encounterScreen = (pkmnNum) => {
+
+    // };
+
+    var img = document.createElement('img');
     fetch('https://pokeapi.co/api/v2/pokemon/' + pkmnNum)
         .then(
             
@@ -57,26 +76,25 @@ function fetchFn (pkmnNum) {
                     // example on how to grab the pokemon's name in this api: console.log(data.names[8].name) = Butterfree;
                     
                     // Grab Pokemon name and ID
-                    pokeNameDiv.innerHTML = "";
+                    let pokeNameDiv = document.createElement("div");
+                    pokeNameDiv.classList.add('pokeNameDiv', 'col-12');
+                    $(pokeNameDiv).addClass('red-border');
+                    pokeIntroDiv.appendChild(pokeNameDiv);
+
                     var pokeId = data.id;
                     var pokeName = data.species.name;
                     var pokeIdNode = document.createTextNode(" (#" + pokeId + ") ");
                     var nameNode = document.createTextNode(pokeName);
                     pokeNameDiv.appendChild(nameNode);
                     pokeNameDiv.appendChild(pokeIdNode);
-                    
-                    // Sprite grab
-                    pokeImgDiv.innerHTML = '';
-                    img.src = data.sprites.front_default;
-                    img.style.width = '300px';
-                    img.style.height = '300px';
-                    pokeImgDiv.appendChild(img);
 
                     // Type Grab
-                    pokeInfoDiv.innerHTML = '';
+                    let pokeTypeDiv = document.createElement('div');
+                    pokeTypeDiv.classList.add('pokeType', 'col-12')
+                    pokeIntroDiv.appendChild(pokeTypeDiv);
+                    
                     var pokeTypeH2 = document.createElement('h2');
                     var pokeTypeSpan = document.createElement('span');
-                    // pokeTypeH2.appendChild(pokeTypeSpan);
                     if (data.types.length > 1) {
                         for (i = 0; i < data.types.length; i++) {
                             var pokeType = data.types[i].type.name;
@@ -90,16 +108,31 @@ function fetchFn (pkmnNum) {
                             pokeTypeSpan.appendChild(pokeTypeText);
                         }
                         pokeTypeH2.appendChild(pokeTypeSpan);
-                        pokeInfoDiv.appendChild(pokeTypeH2);
+                        pokeTypeDiv.appendChild(pokeTypeH2);
                     } else {
                         var pokeType = data.types[0].type.name;
                         var pokeTypeText = document.createTextNode(pokeType);
                         pokeTypeH2.appendChild(pokeTypeText);
-                        pokeInfoDiv.appendChild(pokeTypeH2);
+                        pokeTypeDiv.appendChild(pokeTypeH2);
                     }
+                    
+                    // Sprite grab
+                    let pokeImgDiv = document.createElement("div");
+                    pokeImgDiv.classList.add('pokeImgDiv', 'col-4');
+                    pokeIntroDiv.appendChild(pokeImgDiv);
+
+                    
+                    img.src = data.sprites.front_default;
+                    img.style.width = '300px';
+                    img.style.height = '300px';
+                    pokeImgDiv.appendChild(img);
 
 
                     // Stat name and stat display
+                    let pokeStatsDiv = document.createElement('div');
+                    pokeStatsDiv.classList.add('pokeInfoDiv-stats', 'col-6')
+                    pokeInfoDiv.appendChild(pokeStatsDiv);
+
                     var displayedStatList = document.createElement('ul');
                     displayedStatList.className += " statBox";
                     displayedStatList.style.listStyleType = 'none';
@@ -117,7 +150,7 @@ function fetchFn (pkmnNum) {
                         displayedStat.appendChild(displayedStatNode);
                         displayedStatList.appendChild(displayedStat);
                     }
-                    pokeInfoDiv.appendChild(displayedStatList);
+                    pokeStatsDiv.appendChild(displayedStatList);
 
                     // This fetch is grabbing the pokemon species id from the previous data so we can look for
                     // the right flavor text to display. We then have to iterate through the array of texts
@@ -139,11 +172,15 @@ function fetchFn (pkmnNum) {
                                         // Now we've gotta display the flavor text adjacent to the UL
                                         // and that should be it for the intro box.
                                         console.log(engFlavorText);
+
+                                        let pokeFlavorText = document.createElement('div');
+                                        pokeFlavorText.classList.add('pokeInfoDiv-flvr-text', 'col-6')
+                                        pokeInfoDiv.appendChild(pokeFlavorText);
+
                                         var displayedFlavorText = document.createElement("div");
                                         var displayedFlavorTextNode = document.createTextNode(engFlavorText);
                                         displayedFlavorText.appendChild(displayedFlavorTextNode);
-                                        displayedFlavorText.className += " flavorBox";
-                                        pokeInfoDiv.appendChild(displayedFlavorText);
+                                        pokeFlavorText.appendChild(displayedFlavorText);
                                         // Add stretch goal: Some flavor texts and names therein are formatted weirdly 
                                         // in the API and we should try to correct it for better appearance
                                         return;
@@ -152,8 +189,10 @@ function fetchFn (pkmnNum) {
                             })
                         }
                     )
-                    
+                    pokeInfoDiv.classList.add('col-8');
+                    pokeIntroDiv.appendChild(pokeInfoDiv);
                 });
             }
             
 )}
+});
